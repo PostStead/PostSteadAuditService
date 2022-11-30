d6513462303bcc74652e6ab8def2a5c7f7201466
Alecsandu <45858741+Alecsandu@users.noreply.github.com>
2022-11-30 23:02:50 +0200

    info: Project init commit

---
+++ src/main/java/fmi/poststead/poststeadauditservice/PostSteadAuditServiceApplication.java
@@ -0,0 +1,13 @@
+package fmi.poststead.poststeadauditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSteadAuditServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostSteadAuditServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fmi/poststead/poststeadauditservice/PostSteadAuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fmi.poststead.poststeadauditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSteadAuditServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
