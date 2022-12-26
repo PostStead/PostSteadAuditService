841a45fc8af2734a669c619f50f23286b86858bc
Alecsandu <alecsandu678@gmail.com>
2022-12-26 22:12:40 +0200

    info: Improved the RabbitMQ listener.
    Also fixed the exchange - queue - routing_key details.

--- src/main/java/io/poststead/poststeadauditservice/subscriber/PostSteadNotificationSubscriber.java
+++ src/main/java/io/poststead/poststeadauditservice/subscriber/PostSteadNotificationSubscriber.java
@@ -8,7 +8,8 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.time.OffsetDateTime;
 import java.util.Arrays;
 
 @Slf4j
@@ -20,16 +21,22 @@ public class PostSteadNotificationSubscriber {
     @RabbitListener(
             bindings = @QueueBinding(
                     value = @Queue(
-                            name = "io.poststead.events"
+                            name = "io.poststead.queues.events",
+                            durable = "true"
                     ),
                     exchange = @Exchange(
-                            name = "io.poststead"
-                    )
+                            name = "io.poststead.exchange"
+                    ),
+                    key = "io.poststead.audit"
             )
     )
     public void processNotification(String eventInfo) {
-        try (PrintWriter writer = new PrintWriter("db.txt")) {
-            writer.println(eventInfo);
+        try (FileWriter writer = new FileWriter("db.txt", true)) {
+            writer.write("At ");
+            writer.write(OffsetDateTime.now().toString());
+            writer.write(": ");
+            writer.write(eventInfo);
+            writer.write("\n");
         } catch (Exception e) {
             System.out.println("\n-----------------------------------ERROR-----------------------------------\n");
             System.out.println("Message: " + e.getMessage() + "\n");
